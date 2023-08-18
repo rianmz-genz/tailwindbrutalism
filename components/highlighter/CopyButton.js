4ae8e39dc710421ceb921c16ca89e9b87e11d8ab
Taufik-H <taufikrpl1@gmail.com>
2023-08-18 23:23:17 +0700

    copy button

--- components/highlighter/CopyButton.js
+++ components/highlighter/CopyButton.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CopyButton() {
+function CopyButton({ code }) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(code);
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 2000); // Reset after 2 seconds
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
   return (
     <div>
-      <div className=" bg-slate-800 w-32 relative h-10 rounded-lg  ">
-        <button className="absolute justify-center gap-3 text-lg font-bold hover:-top-0 transition-all duration-200 hover:bg-slate-400 hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-300 border w-53 rounded-lg border-slate-800">
-          Copy
+      <div className="bg-slate-800 w-32 relative h-10 rounded-lg">
+        <button
+          onClick={handleCopy}
+          className="absolute justify-center gap-3 text-lg font-bold hover:-top-0 transition-all duration-200 hover:bg-slate-400 hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-300 border w-53 rounded-lg border-slate-800"
+        >
+          {isCopied ? "Copied!" : "Copy"}
         </button>
       </div>
     </div>
