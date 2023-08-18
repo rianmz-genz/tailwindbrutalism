d2a094d1f23347a8c84b8498ab72411c451a25f7
Taufik-H <taufikrpl1@gmail.com>
2023-08-18 23:23:28 +0700

    responsive button

--- components/highlighter/ResponsiveButton.js
+++ components/highlighter/ResponsiveButton.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ResponsiveButton() {
+function ResponsiveButton({ onSizeChange }) {
+  const [activeSize, setActiveSize] = useState("full");
+
+  const handleSizeChange = (size) => {
+    setActiveSize(size);
+    onSizeChange(size);
+  };
+
   return (
     <div className="flex gap-2">
-      <div className=" bg-slate-800 w-32 relative h-10 rounded-lg  ">
-        <button className="absolute justify-center gap-3 text-lg font-bold hover:-top-0 transition-all duration-200 hover:bg-slate-400 hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-300 border w-53 rounded-lg border-slate-800">
-          Mobile
-        </button>
-      </div>
-      <div className=" bg-slate-800 w-32 relative h-10 rounded-lg  ">
-        <button className="absolute justify-center gap-3 text-lg font-bold hover:-top-0 transition-all duration-200 hover:bg-slate-400 hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-300 border w-53 rounded-lg border-slate-800">
-          Tablet
-        </button>
-      </div>
-      <div className=" bg-slate-800 w-32 relative h-10 rounded-lg  ">
-        <button className="absolute justify-center gap-3 text-lg font-bold hover:-top-0 transition-all duration-200 hover:bg-slate-400 hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center bg-slate-300 border w-53 rounded-lg border-slate-800">
-          Desktop
-        </button>
-      </div>
+      {["xs", "sm", "md", "lg", "full"].map((size) => (
+        <div key={size} className="bg-slate-800 w-32 relative h-10 rounded-lg">
+          <button
+            onClick={() => handleSizeChange(size)}
+            className={`absolute justify-center gap-3 text-lg font-bold hover:-top-0 transition-all duration-200 ${
+              activeSize === size ? "bg-slate-400" : "bg-slate-300"
+            } hover:-left-0 -top-1 -left-1 w-full h-full p-2 flex items-center border w-53 rounded-lg border-slate-800`}
+          >
+            {size}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
