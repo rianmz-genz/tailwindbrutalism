0cc3eec8809ca2056dcc233eb984cc55699e86bd
rianmz-genz <adrianji1945@gmail.com>
2023-08-19 10:28:47 +0700

    remove nunito

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,9 @@
 import "./globals.css";
-import { Nunito } from "next/font/google";
-
-const nunito = Nunito({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Tailwindbrutalism",
-  description: "OpenSource components neubrutalism for TailwindCss",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
